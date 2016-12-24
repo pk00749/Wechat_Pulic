7fc6186861b0533826910c8f289ca0db30f4ab77
warcraftyork <https://github.com/pk00749/>
2016-12-24 13:36:45 +0800

    reply

--- weixinInterface.py
+++ weixinInterface.py
@@ -29,3 +29,16 @@ class WeixinInterface:
 
         if hashcode == signature:  # 如果是来自微信的请求，则回复echostr
             return echostr
+
+    def POST(self):
+        str_xml = web.data() #获得post来的数据
+        xml = etree.fromstring(str_xml)#进行XML解析
+        msgType=xml.find("MsgType").text
+        fromUser=xml.find("FromUserName").text
+        toUser=xml.find("ToUserName").text
+        if msgType == 'text':
+            content = xml.find("Content").text
+        if content == 'help':
+            return self.render.reply_text(fromUser, toUser, int(time.time()), "随便看看？（对不起我功能有限QAQ）")
+        else:
+            return self.render.reply_text(fromUser, toUser, int(time.time()), "哎呀出错了 输入个help看看如何正确的调戏我？")
